8bb1560cc5027dbf22b93177598c304d423e596d
SW <dagedieb@gmail.com>
2020-01-15 16:17:26 +0100

    first commit

---
+++ instAll.sh
@@ -0,0 +1,39 @@
+make_env(){
+  screen -S $1 
+  cd $1 
+  python -m venv venv
+  pip install -r requirements.txt
+}
+
+make_screen_session(){
+  screen -d -m -S $1 bash -c $2
+}
+
+make_mvn(){
+  cd $1 
+  mvn package
+  cd ..
+}
+
+git clone https://github.com/c0ntradicti0n/CorpusCook.git
+make_screen(CorpusCook, make_env(CorpusCook))
+git clone https://github.com/c0ntradicti0n/CorpusCookApp.git
+make_screen(CorpusCookApp, make_env(CorpusCookApp))
+#git clone https://github.com/c0ntradicti0n/Distinctiopus4.git
+git clone https://github.com/c0ntradicti0n/ScienceMap.git
+make_screen(ScienceMap, make_env(ScienceMap))
+git clone https://github.com/c0ntradicti0n/allennlp_vs_ampligraph.git
+make_env(allennlp_vs_ampligraph)
+
+git clone https://github.com/c0ntradicti0n/view_control_web.git
+make_mvn(view_control_web)
+git clone https://github.com/c0ntradicti0n/hal.git
+make_mvn(hal)
+
+wget http://apache.lauf-forum.at/tomcat/tomcat-9/v9.0.30/bin/apache-tomcat-9.0.30.tar.gz
+tar -xzvf apache-tomcat-9.0.30.tar.gz
+
+
+
+
+
