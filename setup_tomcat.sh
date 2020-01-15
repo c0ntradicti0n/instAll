932688b32ab5caf3be247d3f24a9448cc0fd1754
SW <dagedieb@gmail.com>
2020-01-15 17:02:39 +0100

    first commit

--- setup_tomcat.sh
+++ setup_tomcat.sh
@@ -1,5 +1,5 @@
 wget http://apache.lauf-forum.at/tomcat/tomcat-9/v9.0.30/bin/apache-tomcat-9.0.30.tar.gz
 tar -xzvf apache-tomcat-9.0.30.tar.gz
-cp view_web_control/target/*.war apache-tomcat-9.0.30/webapps/
+cp view_control_web/target/CCpy4j-0.0.0.war apache-tomcat-9.0.30/webapps/
 
 
