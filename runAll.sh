09cf16bc45e112a28a70f5a3d2e8ebd24c98bcb7
SW <dagedieb@gmail.com>
2020-01-15 19:51:50 +0100

    first commit

--- runAll.sh
+++ runAll.sh
@@ -1,14 +1,14 @@
-screen -dmS cc
-screen -S cc -p 0 -X stuff 'cd CorpusCook && . venv/bin/activate && python server/annotation_server.py\n'
+screen -dmS CorpusCook
+screen -S  CorpusCook -p 0 -X stuff 'cd CorpusCook && . venv/bin/activate && python server/annotation_server.py\n'
 
-screen -dmS app
-screen -S cc -p 0 -X stuff 'cd CorpusCookApp && . venv/bin/activate && python client_as_micviz.py\n'
+screen -dmS AppCorpusCook
+screen -S AppCorpusCook -p 0 -X stuff 'cd CorpusCookApp && . venv/bin/activate && python client_as_micviz.py\n'
 
-screen -dmS app
-screen -S doc -p 0 -X stuff 'cd pdfetc2txt && . venv/bin/activate && python rest_server.py\n'
+screen -dmS Doc
+screen -S Doc -p 0 -X stuff 'cd pdfetc2txt && . venv/bin/activate && python rest_server.py\n'
 
-screen -dmS app
-screen -S science -p 0 -X stuff 'cd ScienceMap && . venv/bin/activate && python rest_science_map.py\n'
+screen -dmS Science
+screen -S Science -p 0 -X stuff 'cd ScienceMap && . venv/bin/activate && python rest_science_map.py\n'
 
 apache-tomcat-9.0.30/bin/startup.sh
 
