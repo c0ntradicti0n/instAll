05eedfc34b9ee41205f146a5765decfd021ab592
SW <dagedieb@gmail.com>
2020-01-16 16:07:22 +0100

    ke

--- instAll.sh
+++ instAll.sh
@@ -2,8 +2,8 @@ make_env(){
   cd $1 
   python3 -m venv venv
   . venv/bin/activate
-  pip install --upgrade pip
-  pip install -r requirements.txt
+  pip3 install --upgrade pip
+  pip3 install -r requirements.txt
   deactivate
   cd ..
 }
